9e225a252c5a36bc5d12098186c726e48bb8eb01
Jignesh More <jigneshmore5445@gmail.com>
2026-02-07 11:36:54 -0500

    Tested code providing 99% accuracy

--- src/exception.py
+++ src/exception.py
@@ -13,8 +13,8 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message(error_message,error_detail=error_detail)
+        super().__init__(str(error_message))
+        self.error_message = str(error_message)
 
     def __str__(self):
         return self.error_message
